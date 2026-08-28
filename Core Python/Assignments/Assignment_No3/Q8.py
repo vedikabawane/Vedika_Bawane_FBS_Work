f01ce653734611d4a0d4e7de4ed5587f5b7bd99a
vedikabawane <vedikabawane70@gmail.com>
2026-08-28 22:48:48 +0530

    first  commit

--- Core Python/Assignments/Assignment_No3/Q8.py
+++ Core Python/Assignments/Assignment_No3/Q8.py
@@ -3,3 +3,22 @@
 # same. If user enters the same number then show him success message otherwise 
 # failed. (Something like captcha) 
 
+import random
+
+userid = input('Enter User ID: ')
+password = input('Enter Password: ')
+
+if userid == 'admin' and password == '1234':
+    
+    number = random.randint(1000, 9999)
+    print('Your verification number is:', number)
+    
+    entered_number = int(input('Enter the verification number: '))
+    
+    if entered_number == number:
+        print('Success')
+    else:
+        print('Failed')
+        
+else:
+    print('Invalid User ID or Password')
---
+++ Core Python/Assignments/Assignment_No4/Q1.py
@@ -0,0 +1,7 @@
+# WAP to print all even numbers until n.
+
+n = int(input("Enter the number: "))
+
+for i in range(1, n+1):
+    if i % 2 == 0:
+        print(i)

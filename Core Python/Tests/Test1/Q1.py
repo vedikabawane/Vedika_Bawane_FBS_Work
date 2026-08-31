91f8d6aaa04ee98894da4c45a25a94e5d48fa073
vedikabawane <vedikabawane70@gmail.com>
2026-08-31 16:25:45 +0530

    first commit

--- Core Python/Tests/Test1/Q1.py
+++ Core Python/Tests/Test1/Q1.py
@@ -1,3 +1,18 @@
 # Write a program to find the area and perimeter of following figure (Accept the
 # length, breadth and radius from user:
 
+l=int(input('Enter length:'))
+b=int(input('Enter breadth:'))
+r=int(input('Enter radius:'))
+
+#area
+area_rectangle=l*b
+area_circle=3.14*r**2/2
+Total_area=area_rectangle+area_circle
+
+#perimeter
+P_rectangle=2*(l+b)
+C_circle=3.14*r
+Total_perimeter=P_rectangle+C_circle
+
+print(f'Total area is {Total_area} & perimeter is {Total_perimeter}')

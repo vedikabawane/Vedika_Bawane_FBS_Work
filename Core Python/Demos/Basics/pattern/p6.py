5e277a68b30ff5946912965f1ca288be6f196366
vedikabawane <vedikabawane70@gmail.com>
2026-09-01 12:34:27 +0530

    commit all

---
+++ Core Python/Demos/Basics/pattern/p6.py
@@ -0,0 +1,17 @@
+# for i in range(1,6):
+#     for j in range(1,6):
+#         if((i+j)%2==0):
+#             print('*',end=' ')
+#         else:
+#             print('$',end=' ')      
+#     print()
+
+
+for i in range(1,6):
+    for j in range(1,6):
+        if((i+j)%2!=0):
+            print('*',end=' ')
+        else:
+            print('$',end=' ')      
+    print()
+       

dce311a353778e3c63ebfc0c3a2645756c17760d
vedikabawane <vedikabawane70@gmail.com>
2026-09-14 23:46:47 +0530

    commit all

--- Core Python/Assignments/Assignment_No8(Recursive)/Q8.py
+++ Core Python/Assignments/Assignment_No8(Recursive)/Q8.py
@@ -1,2 +1,21 @@
-# Write a program to check whether a number is prime or not using recursion. 
+# Write a program to check whether a number is prime or not using recursion.
 
+def checkPrime(n, i):
+    if(n <= 1):
+        return False
+    elif(i == n):
+        return True
+    elif(n % i == 0):
+        return False
+    else:
+        return checkPrime(n, i + 1)
+
+
+n = int(input('Enter number:'))
+
+res = checkPrime(n, 2)
+
+if(res):
+    print('Number is prime.')
+else:
+    print('Number is not prime.')
